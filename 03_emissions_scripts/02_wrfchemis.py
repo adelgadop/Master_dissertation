2cde389efcc5574ea3df9ac93321022021fb2ff0
Alejandro Delgado <adelgado@iag.usp.br>
2021-08-13 19:59:55 -0500

    MDA8 spatial map

---
+++ 03_emissions_scripts/02_wrfchemis.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 13 11:47:15 2021
+
+@author: adelgado
+"""
+
+# import necessary libraries
+
+import numpy as np
+import xarray as xr
+import pandas as pd
+from matplotlib import pyplot as plt
+from matplotlib import cm
+import seaborn as sns
+
+#%% Import NetCDF files
